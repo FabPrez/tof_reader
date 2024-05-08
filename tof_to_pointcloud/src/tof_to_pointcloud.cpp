c651aa99155989916c67a4354e79daa4b004ecc2
FabPrez <giuseppefabio.preziosa@polimi.it>
2024-05-08 10:51:38 +0200

    chore: Rename package and update launch file and code to reflect changes

--- tof_to_pointcloud/src/tof_to_pointcloud.cpp
+++ tof_to_pointcloud/src/tof_to_pointcloud.cpp
@@ -2,14 +2,12 @@
 
 Tof_to_pointcloud::Tof_to_pointcloud(ros::NodeHandle &nh)
 {
-    sub_distance_tof = nh.subscribe("/tof_pointcloud_data", 2, &Tof_to_pointcloud::tof_pointcloud_to_pcd, this);
+    sub_distance_tof = nh.subscribe("/tof_raw_pointcloud_data", 2, &Tof_to_pointcloud::tof_pointcloud_to_pcd, this);
     pub_pointcloud = nh.advertise<sensor_msgs::PointCloud2>("/tof_pointcloud", 2);
 }
 
 void Tof_to_pointcloud::tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::ConstPtr &msg)
 {
-    // genero pointcloud a partire dall'array di distanze
-
     // popolo la pointcloud
     tof_pointcloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
     tof_pointcloud->width = 1;
@@ -26,11 +24,6 @@ void Tof_to_pointcloud::tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::C
         tof_pointcloud->points[idx].y /= 1000; // converto da m a mm
         tof_pointcloud->points[idx].z /= 1000; // converto da m a mm
     }
-
-    ROS_INFO("tof_pointcloud 0,0,0: %f, %f, %f", tof_pointcloud->points[0].x, tof_pointcloud->points[0].y, tof_pointcloud->points[0].z);
-    ROS_INFO("tof_pointcloud 1,1,1: %f, %f, %f", tof_pointcloud->points[1].x, tof_pointcloud->points[1].y, tof_pointcloud->points[1].z);
-    ROS_INFO("tof_pointcloud 2,2,2: %f, %f, %f", tof_pointcloud->points[2].x, tof_pointcloud->points[2].y, tof_pointcloud->points[2].z);
-    ROS_INFO("tof_pointcloud last,last,last: %f, %f, %f", tof_pointcloud->points[msg->data.size() / 3 - 1].x, tof_pointcloud->points[msg->data.size() / 3 - 1].y, tof_pointcloud->points[msg->data.size() / 3 - 1].z);
 
     // converto la pointcloud in messaggio ROS
     pcl::toROSMsg(*tof_pointcloud, pointcloud_msg);
