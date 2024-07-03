74e710922b56a75ab4bce0738c10d57b228b58cc
FabPrez <giuseppefabio.preziosa@polimi.it>
2024-07-03 11:44:58 +0200

    first commit for this branch of multiple tof - it seems that the two tofs are not well integrated

--- read_raw_tof_pointcloud/src/read_raw_tof_pointcloud.py
+++ read_raw_tof_pointcloud/src/read_raw_tof_pointcloud.py
@@ -7,13 +7,14 @@ import struct
 
 # Constants and configuration
 SERIAL_PORT = '/dev/ttyACM0'
+TOF_NUMBER = 0
 TYPE_BIT_SENT_BY_TOF = 16 # TOF IS SENDING 16 BIT PER DATA: 2 BYTES
 TOF_ZONE_NUMBER = 64
 POINTS_PER_ZONE = 3 # X, Y, Z COORDINATES
 BYTE_PER_POINT = 2 # TOF IS USING 2 BYTES PER POINT
-BYTE_PER_READ = TOF_ZONE_NUMBER * POINTS_PER_ZONE * BYTE_PER_POINT
 BAUD_RATE = 115200
 TIMEOUT = 1
+BYTE_PER_READ = TOF_ZONE_NUMBER * POINTS_PER_ZONE * BYTE_PER_POINT + (TOF_NUMBER+1)*2 # 2 BYTES FOR THE TOF NUMBER
 
 def combine_bytes(msb, lsb):
     # COMBINE THE TWO BYTES TO FORM A 16-BIT INTEGER
@@ -35,10 +36,10 @@ def read_tof_pointcloud():
     # Verifica se la lunghezza di value è esattamente BYTE_PER_READ byte
     if len(value) == BYTE_PER_READ:
         # JUST FOR DEBUGGING --------------------------------------
-        # rospy.loginfo("Valori dei byte ricevuti:-----------")
-        # for byte in value:
-        #     rospy.loginfo(format(byte, '08b'))
-        # rospy.loginfo("Valori dei byte terminati-----------")
+        rospy.loginfo("Valori dei byte ricevuti:-----------")
+        for byte in value:
+            rospy.loginfo(format(byte, '08b'))
+        rospy.loginfo("Valori dei byte terminati-----------")
         
         interi = []
         for i in range(0, len(value), BYTE_PER_POINT):  # Leggi i byte in coppia
@@ -48,12 +49,24 @@ def read_tof_pointcloud():
                 intero = struct.unpack('<h', struct.pack('<H', intero))[0]
             
             interi.append(intero)
-        
+            
+        array_to_publish = interi[:-1]
+        # array_to_publish = interi
         # Publish the array of integers as Int32MultiArray
-        array_msg = Int32MultiArray(data=interi)
-        pub.publish(array_msg)
+        array_msg = Int32MultiArray(data=array_to_publish)
+        
+        rospy.loginfo("Valori ultimo intero---")
+        rospy.loginfo(interi[-1])
+        rospy.loginfo("-----------")
+            
+        if interi[-1] == TOF_NUMBER:
+            rospy.loginfo(f"TOF NUMBER: {TOF_NUMBER}, Is Publishing!")
+            pub.publish(array_msg)
 
 if __name__ == '__main__': 
+    # TOF_NUMBER = rospy.get_param('/tof_to_pointcloud/tof_number')
+    # rospy.loginfo(f"Il valore di my_param è: {TOF_NUMBER}")
+    
     rospy.init_node('TOF_POINTCLODUD_READER')
     try:
         ser = serial.Serial(SERIAL_PORT, BAUD_RATE, 8, 'N', 1, timeout=TIMEOUT)
@@ -65,72 +78,10 @@ if __name__ == '__main__':
     rate = rospy.Rate(10) 
     
     rospy.loginfo("Start reading and publishing data")
+    
+    
     while not rospy.is_shutdown():
         read_tof_pointcloud()
         rate.sleep()
     ser.close()
-
-# def combine_bytes(msb, lsb):
-#     # Combina i due byte per formare un intero a 16 bit
-#     combined_value = (msb << 8) | lsb
-#     return combined_value
-
-# def is_negative(value):
-#     # Controlla il bit più significativo
-#     if value & 0x8000:  # Se il bit più significativo è impostato
-#         return True    # Il numero è negativo
-#     else:
-#         return False   # Il numero è positivo o zero
-
-# def read_tof_pointcloud():
-
-#     ser.flushInput()
-#     ser.write(b'1')
-#     value = ser.read(128*3)  
-#     # Leggi esattamente 128*3 byte, ogni numero è codificato su 2 byte (quindi 64*2 = 128)
-#     # e ci sono 3 numeri (distanza x, y, z) 
-    
-#     # Verifica se la lunghezza di value è esattamente 128*3 byte
-#     if len(value) == 128*3:
-#         # rospy.loginfo("Valori dei byte ricevuti:-----------")
-#         # for byte in value:
-#         #     rospy.loginfo(format(byte, '08b'))
-#         # rospy.loginfo("Valori dei byte terminati-----------")
-        
-#         # Converti i byte in un array di interi
-#         interi = []
-#         for i in range(0, len(value), 2):  # Leggi i byte in coppia
-#             # Inverti l'ordine dei byte e combina i byte in un intero a 16 bit
-#             intero = combine_bytes(value[i+1], value[i])
-            
-#             # Controlla se il numero è negativo
-#             if is_negative(intero):
-#                 # Se è negativo, converte il numero negativo a un intero a 32 bit (complemento a due)
-#                 # (per mantenere il segno corretto)
-#                 intero = struct.unpack('<h', struct.pack('<H', intero))[0]
-            
-#             interi.append(intero)
-        
-#         # Pubblica l'array di interi come messaggio Int32MultiArray
-#         array_msg = Int32MultiArray(data=interi)
-#         pub.publish(array_msg)
-
-# if __name__ == '__main__': 
-#     rospy.init_node('tof_distance_publisher')
-#     try:
-#         ser = serial.Serial('/dev/ttyACM0', 115200, 8, 'N', 1, timeout=1)
-#     except serial.SerialException as e:
-#         rospy.logerr("Non si hanno i permessi della porta. Eseguire 'sudo chmod 666 /dev/ttyACM0' per abilitare la lettura.")
-#         exit()
-        
-#     pub = rospy.Publisher('tof_pointcloud_data', Int32MultiArray, queue_size=10)
-
-#     rate = rospy.Rate(10)  # Frequenza di pubblicazione (10 Hz)
-    
-#     rospy.loginfo("Inizio lettura e pubblicazione dei dati")
-#     while not rospy.is_shutdown():
-#         read_tof_pointcloud()
-#         rate.sleep()
-
-#     ser.close()
 
