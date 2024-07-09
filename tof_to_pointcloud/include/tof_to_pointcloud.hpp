3927c80b4734eb0c3a99227ce5072f92065359cf
FabPrez <giuseppefabio.preziosa@polimi.it>
2024-07-09 10:43:01 +0200

    Added coninuos and discrete mode of acquisition

--- tof_to_pointcloud/include/tof_to_pointcloud.hpp
+++ tof_to_pointcloud/include/tof_to_pointcloud.hpp
@@ -2,32 +2,39 @@
 #define TOF_TO_POINTCLOUD_HPP
 
 #include <ros/ros.h>
+#include <std_srvs/Trigger.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <std_msgs/Int32MultiArray.h>
-#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <boost/make_shared.hpp>
 
 class Tof_to_pointcloud
 {
-	public:
-		Tof_to_pointcloud(ros::NodeHandle& nh);
-		void spinner(void);
-	private:
-		void tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::ConstPtr& msg);
+public:
+    Tof_to_pointcloud(ros::NodeHandle& nh);
+	void spinner(void);
 
-        pcl::PointCloud<pcl::PointXYZ>::Ptr tof_pointcloud;
+private:
+    ros::NodeHandle nh;
+    ros::NodeHandle private_nh;        
+    ros::Subscriber sub_distance_tof;
+    ros::Publisher pub_pointcloud;
+    ros::ServiceServer sv_pointcloud;
+	
+    pcl::PointCloud<pcl::PointXYZ>::Ptr tof_pointcloud;
+    sensor_msgs::PointCloud2 pointcloud_msg;
+    bool acquisitionAllowed;
+    bool continuousMode;
+    
+    std::string input_topic;
+    std::string output_topic;
+    std::string frame_id;
 
-		ros::NodeHandle nh;
-		ros::NodeHandle private_nh;		
-		
-		ros::Subscriber sub_distance_tof;
-		ros::Publisher pub_pointcloud;
-		sensor_msgs::PointCloud2 pointcloud_msg;
-
-		std::string input_topic;
-    	std::string output_topic;
-    	std::string frame_id;
+    void tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::ConstPtr& msg);
+    bool startAcquisition(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
 };
 
 #endif // TOF_TO_POINTCLOUD_HPP
+
--- tof_to_pointcloud/src/tof_to_pointcloud.cpp
+++ tof_to_pointcloud/src/tof_to_pointcloud.cpp
@@ -1,21 +1,33 @@
-#include "../include/tof_to_pointcloud.hpp"
+#include "tof_to_pointcloud.hpp"
 
-Tof_to_pointcloud::Tof_to_pointcloud(ros::NodeHandle &nh)
+Tof_to_pointcloud::Tof_to_pointcloud(ros::NodeHandle& nh)
+    : nh(nh), private_nh("~"), acquisitionAllowed(false)
 {
-    this->nh = nh;
-    ros::NodeHandle private_nh("~"); // Private NodeHandle
     private_nh.getParam("input_topic", input_topic);
     private_nh.getParam("output_topic", output_topic);
     private_nh.getParam("frame_id", frame_id);
-    
+    private_nh.getParam("continuous_mode", continuousMode);
+
     sub_distance_tof = nh.subscribe(input_topic, 2, &Tof_to_pointcloud::tof_pointcloud_to_pcd, this);
     pub_pointcloud = nh.advertise<sensor_msgs::PointCloud2>(output_topic, 2);
+
+    if (!continuousMode)
+    {
+        // Service server to start acquisition
+        sv_pointcloud = nh.advertiseService("/start_acquisition", &Tof_to_pointcloud::startAcquisition, this);
+    }
+
+    tof_pointcloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
 }
 
-void Tof_to_pointcloud::tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::ConstPtr &msg)
+void Tof_to_pointcloud::tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::ConstPtr& msg)
 {
-    // popolo la pointcloud
-    tof_pointcloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    if (!acquisitionAllowed && !continuousMode)
+    {
+        return;
+    }
+
+    // Popolo la pointcloud
     tof_pointcloud->width = 1;
     tof_pointcloud->height = msg->data.size() / 3;
     tof_pointcloud->is_dense = false;
@@ -26,20 +38,35 @@ void Tof_to_pointcloud::tof_pointcloud_to_pcd(const std_msgs::Int32MultiArray::C
         tof_pointcloud->points[idx].x = msg->data[3 * idx];
         tof_pointcloud->points[idx].y = msg->data[3 * idx + 1];
         tof_pointcloud->points[idx].z = msg->data[3 * idx + 2];
-        tof_pointcloud->points[idx].x /= 1000; // converto da m a mm
-        tof_pointcloud->points[idx].y /= 1000; // converto da m a mm
-        tof_pointcloud->points[idx].z /= 1000; // converto da m a mm
+        tof_pointcloud->points[idx].x /= 1000; // converto da mm a m
+        tof_pointcloud->points[idx].y /= 1000; // converto da mm a m
+        tof_pointcloud->points[idx].z /= 1000; // converto da mm a m
         tof_pointcloud->points[idx].x *= -1;
     }
-
 
     pcl::toROSMsg(*tof_pointcloud, pointcloud_msg);
     pointcloud_msg.header.frame_id = frame_id;
     pointcloud_msg.header.stamp = ros::Time::now();
+
+    // Reset the flag if in service mode
+    if (!continuousMode)
+    {
+        acquisitionAllowed = false;
+    }
+
+    // Pubblica la pointcloud
     pub_pointcloud.publish(pointcloud_msg);
 }
 
-void Tof_to_pointcloud::spinner()
+bool Tof_to_pointcloud::startAcquisition(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
+{
+    acquisitionAllowed = true;
+    res.success = true;
+    res.message = "Acquisition started";
+    return true;
+}
+
+void Tof_to_pointcloud::spinner(void)
 {
     ros::spinOnce();
 }
